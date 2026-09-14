935f10357d2aece92039ac0a0059ab8326a6bece
JayeshMaliCoder <jayeshmali224@gmail.com>
2026-09-14 11:43:53 +0530

    Fix CORS for delete media

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -35,7 +35,7 @@ func withCORS(next http.Handler, origin string) http.Handler {
 		}
 		w.Header().Set("Access-Control-Allow-Origin", origin)
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, DELETE, OPTIONS")
 		if r.Method == http.MethodOptions {
 			w.WriteHeader(http.StatusNoContent)
 			return
